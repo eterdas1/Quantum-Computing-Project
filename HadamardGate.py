ddb3a1dd24058e9b7df294a703e4212edce44e2c
LoganMackay02 <124052239+LoganMackay02@users.noreply.github.com>
2023-02-06 12:34:37 +0000

    Update HadamardGate.py

--- HadamardGate.py
+++ HadamardGate.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  5 16:24:58 2023
 
-@author: logan
-"""
 import numpy as np
 def Hadamard_Gate(qubitCoeffs):
     #Function takes in an array containing the coefficients associated with a qubit
